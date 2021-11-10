150fe352f15e51d98ecc8b4e388c57feca980726
huma17ax <genbot.think17Virt@gmail.com>
2021-11-10 22:15:55 +0900

    About内容変更

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,101 +1,113 @@
 <template>
-    <div class="whole">
-        <div class="content">
-            <h2>経歴</h2>
-            <table>
-                <tr v-for="data in careerData" v-bind:key="data.id">
-                    <th>{{data.period}}</th>
-                    <td v-html="data.content"></td>
-                </tr>
-            </table>
-        </div>
-        <div class="content">
-            <h2>現在</h2>
-            <div class="detail">
-                千葉大学 工学部 総合工学科 情報工学コース 3年 <br>
-                千葉大学 電子計算機研究会 <br>
-                千葉大学 ノートテイク会
-            </div>
-        </div>
-        <div class="content">
-            <h2>趣味・興味</h2>
-            <div class="detail">
-                <h4>xR</h4>
-                <div class="detail">
-                    現実世界ではありえない表現をしたり，現実を拡張できるxR．<br>
-                    現在VRを用いたゲームを制作中．
-                </div>
-            </div>
-            <div class="detail">
-                <h4>Vtuber/Vsinger</h4>
-                <div class="detail">
-
-                </div>
-            </div>
-            <div class="detail">
-                <h4>EDM</h4>
-                <div class="detail">
-                    ジャンルは"Future Bass"など．
-                </div>
-            </div>
-        </div>
+  <div class="wrapper">
+    <div class="identity">
+      <img class="icon" src="@/assets/image/icon.jpg">
+      <div>
+        <div class="name">関谷 悠冬</div>
+        <div class="belong">千葉大学大学院 融合理工学府 数学情報科学専攻</div>
+      </div>
     </div>
+    <div class="content" v-for="(item, idx) in items" :key="idx">
+      <h2>{{item.title}}</h2>
+      <table>
+        <tr v-for="data in item.history" v-bind:key="data.id">
+          <th>{{ data.period }}</th>
+          <td v-html="data.content"></td>
+        </tr>
+        <tr v-if="idx==0">
+          <th>{{ nowDateStr }}</th>
+          <td v-html="'現在'"></td>
+        </tr>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import careerData from '@/assets/json/history.json'
+import careerData from "@/assets/json/history.json"
+import qualifications from "@/assets/json/qualifications.json"
+import research from "@/assets/json/research.json"
 
 export default {
-  name: 'aboutme',
-  data: function () {
+  name: "About",
+  data () {
     return {
-      careerData: careerData
+      items: [
+        {title: '経歴', history: careerData},
+        {title: '資格・検定', history: qualifications},
+        {title: '論文', history: research}
+      ],
+    }
+  },
+  computed: {
+    nowDateStr () {
+      const date = new Date()
+      return date.getFullYear() + '年' + date.getMonth() + '月'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.whole {
-    height: 100%;
-    margin: auto;
-    overflow-y: auto;
+.wrapper {
+  height: 100%;
+  margin: auto;
+  overflow-y: auto;
+}
+
+.identity {
+  display: flex;
+  height: 75px;
+  position: relative;
+  margin: 16px 0px;
+  .icon {
+    width: 75px;
+    border-radius: 20px;
+  }
+  .name {
+    position: relative;
+    font-size: 2em;
+    top: 10px;
+    left: 20px;
+  }
+  .belong {
+    position: relative;
+    font-size: 1em;
+    top: 2px;
+    left: 20px;
+  }
 }
 
 .content {
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 }
 
 h2 {
-    z-index: 2;
-    position: sticky;
-    top:0px;
-    background: linear-gradient(to right, #FFFFFF,rgba(255, 255, 255, 0));
-    margin: 0;
-}
-
-h4 {
-    margin-bottom: 0;
+  z-index: 2;
+  position: sticky;
+  top: 0px;
+  background: linear-gradient(to right, #ffffff, rgba(255, 255, 255, 0));
+  margin: 0;
 }
 
 table {
-    width: 100%;
-    border: 0px;
-    border-collapse: collapse;
-    border-spacing: 0;
+  width: 100%;
+  border: 0px;
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 
 tr {
-    height: 40px;
-    border-bottom: solid 2px #FFFFFF;
+  height: 40px;
+  border-bottom: solid 2px #ffffff;
 }
 
 th {
-    padding-left: 20px;
-    text-align: left;
-    background-color: $main-color;
-    width: 150px;
-    position: relative;
+  padding-left: 20px;
+  text-align: left;
+  background-color: $main-color;
+  width: 150px;
+  position: relative;
 }
 
 th::after {
@@ -104,21 +116,17 @@ th::after {
   width: 0px;
   height: 0px;
   position: absolute;
-  top:calc(50% - 10px);
-  right:-10px;
+  top: calc(50% - 10px);
+  right: -10px;
   border-left: 10px solid $main-color;
   border-top: 10px solid transparent;
   border-bottom: 10px solid transparent;
 }
 
 td {
-    background-color: #BBBBBB;
-    padding-top: 5px;
-    padding-bottom: 5px;
-    padding-left: 20px;
-}
-
-.detail {
-    padding-left: 20px;
+  background-color: #bbbbbb;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 20px;
 }
 </style>
